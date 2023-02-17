9df7e6b54adfcf623bb06129ae8154465114e1a5
Chandra <chandrakantprasad25@gmail.com>
2023-02-17 13:49:28 +0530

    [Chandss] Refactor: For code optimization.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,7 +9,6 @@ import * as UserService from '../services/user.service';
  */
 export const newUser = async (req, res, next) => {
   try {
-    console.log('in Controller');
     const data = await UserService.newUser(req.body);
     res.status(data.code).json({
       code: data.code,
@@ -29,30 +28,12 @@ export const newUser = async (req, res, next) => {
  */
 export const login = async (req, res, next) => {
   try {
-    const db = await UserService.getAllUsers();
-    const check = db.filter((obj) => req.body.email == obj.email);
-    if (check[0] == null) {
-      res.status(HttpStatus.BAD_REQUEST).json({
-        code: HttpStatus.BAD_REQUEST,
-        data: 'Login Failed',
-        message: 'Invalid User Name'
-      });
-    } else {
-      const passwordCheck = check[0].password;
-      if (passwordCheck == req.body.password) {
-        res.status(HttpStatus.OK).json({
-          code: HttpStatus.OK,
-          data: req.body.email,
-          message: 'Log in Successful'
-        });
-      } else {
-        res.status(HttpStatus.BAD_REQUEST).json({
-          code: HttpStatus.BAD_REQUEST,
-          data: 'Login Failed',
-          message: 'Invalid User Password'
-        });
-      }
-    }
+    const data = await UserService.login(req.body);
+    res.status(data.code).json({
+      code: data.code,
+      data: data.data,
+      message: data.message
+    });
   } catch (error) {
     next(error);
   }
